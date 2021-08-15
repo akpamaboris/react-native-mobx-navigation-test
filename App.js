c015dbe73a62b881b26a03027f994e1f027d7131
Zeliwipin <zeliwipin@gmail.com>
2021-08-15 20:43:52 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
+import { makeAutoObservable } from "mobx";
+import { observer } from "mobx-react";
 
-export default function App() {
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+class Timer {
+  secondsPassed = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increaseTimer() {
+    this.secondsPassed += 1;
+  }
+}
+
+const myTimer = new Timer();
+
+const TimerView = observer(({ timer }) => (
+  <View>
+    <Text>Seconds passed: {myTimer.secondsPassed}</Text>
+  </View>
+));
+
+const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TimerView />
+      <Text>You are on Home Screen</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={() => myTimer.increaseTimer()}>
+        <Text>Press Me</Text>
+      </TouchableOpacity>
+
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          // La fonction navigate() permet de naviguer vers un écran. On lui passe comme argument le nom de l'écran vers lequel on souhaite naviguer (ce nom se trouve dans App.js)
+          onPress={() => navigation.navigate("Details")}
+        />
+      </View>
     </View>
+  );
+};
+
+const Details = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TimerView />
+      <Text>You are on Home Screen</Text>
+      <StatusBar style="auto" />
+      <TouchableOpacity onPress={() => myTimer.increaseTimer()}>
+        <Text>Press Me</Text>
+      </TouchableOpacity>
+
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Home"
+          // La fonction navigate() permet de naviguer vers un écran. On lui passe comme argument le nom de l'écran vers lequel on souhaite naviguer (ce nom se trouve dans App.js)
+          onPress={() => navigation.navigate("Home")}
+        />
+      </View>
+    </View>
+  );
+};
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
